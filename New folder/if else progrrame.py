664a4466db25ce0814af0e2c3becae74e85245a1
rajatprajapati961cmyk <rajatprajapati961@gmail.com>
2026-07-11 23:41:02 +0530

    add new if elif else progrrame

---
+++ New folder/if else progrrame.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+elif age < 15:
+    print("You are not eligible to vote.")
+
+else:
+    print("You are baby please wait for some time.")
