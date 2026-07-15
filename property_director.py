227dfcafd91256e872a9bf308c3af45ae8454720
rajatprajapati961cmyk <rajatprajapati961@gmail.com>
2026-07-15 08:49:33 +0530

    add new propert director

---
+++ property_director.py
@@ -0,0 +1,28 @@
+class Employee:
+    a = 1
+
+    @classmethod
+    def show(cls):
+        print(f"The class Attribute of a is  {cls.a}")
+
+
+    @property
+    def name (self):
+        return f"{self.fname} {self.lanme}"
+    
+
+    @name.setter
+    def name (self,value):
+        self.fname = value.split(" ")[0]
+        self.lname = value.split(" ")[1]
+
+
+    
+
+
+e = Employee()
+e.a = 45
+
+e.show()  # Output: Employee Class Method: a is 1
+e.name = "Rajat Prajapati"
+print(e.fname,e.lname)
